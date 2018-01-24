a13f22f4e645fbe2d80ada345ab867285a51606f
Vitalii Golovko <golovko.inc@gmail.com>
2018-01-24 03:54:39 +0200

    les_7

---
+++ src/les_07/Rowboat.java
@@ -0,0 +1,7 @@
+package les_07;
+
+public class Rowboat extends Boat {
+    public void rowTheBoat(){
+        System.out.print("Давай, Наташа!");
+    }
+}
---
+++ src/les_07/Sailboat.java
@@ -0,0 +1,7 @@
+package les_07;
+
+public class Sailboat extends Boat {
+    public void move (){
+        System.out.print("Поднять паруса!");
+    }
+}
